8a87135b237c7bbb583be979aa8922ac83d6a509
engibous <kyle.engibous@zywave.com>
2019-05-01 19:48:30 -0500

    Removing unused CMS stuff
    First blog post - kubernetes planning

--- src/cms/cms.js
+++
@@ -1,8 +0,0 @@
-import CMS from 'netlify-cms'
-
-
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
